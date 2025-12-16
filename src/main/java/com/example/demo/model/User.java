8c79bb007ee5726bf566cc3408aa5659067c9e6d
bala-amypo <bala@amypo.in>
2025-12-16 14:42:56 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,58 @@
-package com.example.demo.model;
+package com.example.ROI.model;
+
+import java.time.LocalDateTime;
 
 public class User {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+    public User(String fullName, String email, String password, String role, LocalDateTime createdAt) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
     
 }
