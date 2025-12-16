03e2d61ee2000414325a23742f65f72a692fa4ff
bala-amypo <bala@amypo.in>
2025-12-16 14:41:09 +0000

    User commit

--- src/main/java/com/example/demo/model/DiscountCode.java
+++ src/main/java/com/example/demo/model/DiscountCode.java
@@ -1,5 +1,56 @@
-package com.example.demo.model;
+package com.example.R.model;
 
 public class DiscountCode {
+    private Long id;
+    private String code;
+    private Influencer influencer;
+    private Campaign campaign;
+    private Double discountPercentage;
+    private Boolean active;
+    public DiscountCode(String code, Influencer influencer, Campaign campaign, Double discountPercentage,
+            Boolean active) {
+        this.code = code;
+        this.influencer = influencer;
+        this.campaign = campaign;
+        this.discountPercentage = discountPercentage;
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getCode() {
+        return code;
+    }
+    public Influencer getInfluencer() {
+        return influencer;
+    }
+    public Campaign getCampaign() {
+        return campaign;
+    }
+    public Double getDiscountPercentage() {
+        return discountPercentage;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public void setInfluencer(Influencer influencer) {
+        this.influencer = influencer;
+    }
+    public void setCampaign(Campaign campaign) {
+        this.campaign = campaign;
+    }
+    public void setDiscountPercentage(Double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    
     
 }
