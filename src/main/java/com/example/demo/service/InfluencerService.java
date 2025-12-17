58f732f402e112cbee48c5bb0b6695501fb05e2e
bala-amypo <bala@amypo.in>
2025-12-17 04:53:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/InfluencerService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.ROI.model.Influencer;
+
+public interface InfluencerService {
+    Influencer creatInfluencer(Influencer influencer);
+    Influencer updatInfluencer(Long id,Influencer influencer);
+    Influencer getInfluencer(Long id);
+    List<Influencer>getAllInfluencers();
+    void deactivateInfluencer(Long id);
+
+
+}
