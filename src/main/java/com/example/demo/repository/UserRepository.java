07fac7b606126c74efa6c457722d00c7400341d1
bala-amypo <bala@amypo.in>
2025-12-17 08:41:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
+
+import com.example.demo.model.User;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
     
