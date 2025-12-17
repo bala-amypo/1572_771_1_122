a0f3c93be9bc4be25a02e45e81c7b406c9348687
bala-amypo <bala@amypo.in>
2025-12-17 08:42:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/SaleTransactionRepository.java
+++ src/main/java/com/example/demo/repository/SaleTransactionRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.SaleTransaction;
+import com.example.demo.model.User;
 @Repository
-public interface SaleTransactionRepository extends JpaRepository<SaleTransaction,Long>{
+public interface UserRepository extends JpaRepository<User,Long> {
 
     
-}
+} 
