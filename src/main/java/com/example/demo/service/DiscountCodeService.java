bafef7f1fa85a8f49a725f1121f74deed0fb0707
bala-amypo <bala@amypo.in>
2025-12-17 04:52:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DiscountCodeService.java
@@ -0,0 +1,14 @@
+package com.example.ROI.service;
+
+import com.example.ROI.model.DiscountCode;
+
+public interface DiscountCodeService {
+    DiscountCode creaDiscountCode(DiscountCode code);
+    DiscountCode updaDiscountCode(Long id,DiscountCode code);
+    DiscountCode getCodeById(Long id);
+    DiscountCode getCodesByInfluencer(Long influencerId);
+    DiscountCode getCodesByCampaign(Long campaignId);
+    void deactivateCode(Long id);
+
+    
+} 
