25c7521d59ce19f62d25c95003898cae41920f65
bala-amypo <bala@amypo.in>
2025-12-17 08:44:13 +0000

    User commit

--- src/main/java/com/example/demo/service/CampaignService.java
+++ src/main/java/com/example/demo/service/CampaignService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import java.util.List;
 
-import com.example.ROI.model.Campaign;
+import com.example.demo.model.Campaign;
 
 public interface CampaignService {
     Campaign createCampaign(Campaign campaign);
