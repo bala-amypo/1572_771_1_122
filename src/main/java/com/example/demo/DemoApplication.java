f79544fb1efd7db98a78745d8890722c77a6f28d
bala-amypo <bala@amypo.in>
2025-12-18 17:27:36 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,9 @@
-package com.example.demo;   
+package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan("com.example.ROI.model")
-@EnableJpaRepositories("com.example.ROI.repository")
-@ComponentScan("com.example.ROI")
 public class DemoApplication {
 
     public static void main(String[] args) {
