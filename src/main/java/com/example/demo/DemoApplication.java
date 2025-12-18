e068ff6d7955e699db63c48d0acfacd16f1e6f5a
bala-amypo <bala@amypo.in>
2025-12-18 17:26:03 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,4 @@
-package com.example.demo;   // keep your current package
-
+package com.example.demo;   
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
