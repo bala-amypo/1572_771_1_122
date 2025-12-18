e50236327fe74675c94d5bf86407cb6c26ddc2c8
bala-amypo <bala@amypo.in>
2025-12-18 08:31:14 +0000

    User commit

--- src/main/java/com/example/demo/service/InfluencerService.java
+++ src/main/java/com/example/demo/service/InfluencerService.java
@@ -1,15 +1,17 @@
 package com.example.demo.service;
 
 import java.util.List;
-
 import com.example.demo.model.Influencer;
 
 public interface InfluencerService {
+
     Influencer creatInfluencer(Influencer influencer);
-    Influencer updatInfluencer(Long id,Influencer influencer);
-    Influencer getInfluencer(Long id);
-    List<Influencer>getAllInfluencers();
+
+    Influencer updatInfluencer(Long id, Influencer influencer);
+
+    Influencer getInfluencerById(Long id);
+
+    List<Influencer> getAllInfluencers();
+
     void deactivateInfluencer(Long id);
-
-
 }
