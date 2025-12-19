8ecc42dba84110c2c405ee29a4b75a2ca18d2add
bala-amypo <bala@amypo.in>
2025-12-19 05:26:05 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CampaignServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CampaignServiceImpl.java
@@ -1,62 +1,60 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.model.Campaign;
-// import com.example.demo.repository.CampaignRepository;
-// import com.example.demo.service.CampaignService;
+import com.example.demo.model.Campaign;
+import com.example.demo.repository.CampaignRepository;
+import com.example.demo.service.CampaignService;
 
-// @Service
-// public class CampaignServiceImpl implements CampaignService {
+@Service
+public class CampaignServiceImpl implements CampaignService {
 
-//     @Autowired
-//     private CampaignRepository campaignRepository;
+    @Autowired
+    CampaignRepository campaignRepository;
 
-//     @Override
-//     public Campaign createCampaign(Campaign campaign) {
-//         return campaignRepository.save(campaign);
-//     }
+    @Override
+    public Campaign createCampaign(Campaign campaign) {
+        return campaignRepository.save(campaign);
+    }
 
-//     @Override
-//     public Campaign updateCampaign(Long id, Campaign updatedCampaign) {
-//         Optional<Campaign> optionalCampaign = campaignRepository.findById(id);
+    @Override
+    public List<Campaign> getAllCampaigns() {
+        return campaignRepository.findAll();
+    }
 
-//         if (optionalCampaign.isPresent()) {
-//             Campaign oldCampaign = optionalCampaign.get();
+    @Override
+    public Campaign getCampaignById(Long id) {
+        Optional<Campaign> optionalCampaign = campaignRepository.findById(id);
+        return optionalCampaign.orElse(null);
+    }
 
-//             oldCampaign.setCampaignName(updatedCampaign.getCampaignName());
-//             oldCampaign.setStartDate(updatedCampaign.getStartDate());
-//             oldCampaign.setEndDate(updatedCampaign.getEndDate());
-//             oldCampaign.setBudget(updatedCampaign.getBudget());
-//             oldCampaign.setActive(updatedCampaign.getActive());
+    @Override
+    public boolean deactivateCampaign(Long id) {
+        if (campaignRepository.existsById(id)) {
+            Campaign campaign = campaignRepository.findById(id).get();
+            campaign.setActive(false);
+            campaignRepository.save(campaign);
+            return true;
+        }
+        return false;
+    }
 
-//             return campaignRepository.save(oldCampaign);
-//         }
-//         return null;
-//     }
+    @Override
+    public Campaign updateCampaign(Long id, Campaign updatedCampaign) {
+        Optional<Campaign> optionalCampaign = campaignRepository.findById(id);
 
-//     @Override
-//     public Campaign getCampaignById(Long id) {
-//         return campaignRepository.findById(id).orElse(null);
-//     }
-
-//     @Override
-//     public List<Campaign> getAllCampaigns() {
-//         return campaignRepository.findAll();
-//     }
-
-//     @Override
-//     public void deactivateCampaign(Long id) {
-//         Optional<Campaign> optionalCampaign = campaignRepository.findById(id);
-
-//         if (optionalCampaign.isPresent()) {
-//             Campaign campaign = optionalCampaign.get();
-//             campaign.setActive(false);
-//             campaignRepository.save(campaign);
-//         }
-//     }
-// }
+        if (optionalCampaign.isPresent()) {
+            Campaign oldCampaign = optionalCampaign.get();
+            oldCampaign.setCampaignName(updatedCampaign.getCampaignName());
+            oldCampaign.setStartDate(updatedCampaign.getStartDate());
+            oldCampaign.setEndDate(updatedCampaign.getEndDate());
+            oldCampaign.setBudget(updatedCampaign.getBudget());
+            return campaignRepository.save(oldCampaign);
+        }
+        return null;
+    }
+}
