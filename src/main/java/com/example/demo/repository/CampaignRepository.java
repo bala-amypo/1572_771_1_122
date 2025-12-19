304a9ad936f0557f22c334038a4584a4ce78d253
bala-amypo <bala@amypo.in>
2025-12-19 16:05:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/CampaignRepository.java
+++ src/main/java/com/example/demo/repository/CampaignRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Campaign;
-@Repository
-public interface CampaignRepository extends JpaRepository<Campaign,Long>{
 
-    
-} 
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+}
