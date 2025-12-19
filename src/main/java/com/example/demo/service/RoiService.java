0cde46da43ae82abd225487ce5d413f194e3249d
bala-amypo <bala@amypo.in>
2025-12-19 09:34:05 +0000

    User commit

--- src/main/java/com/example/demo/service/RoiService.java
+++ src/main/java/com/example/demo/service/RoiService.java
@@ -1,11 +1,16 @@
-// package com.example.demo.service;
+package com.example.demo.service;
 
-// import com.example.demo.model.RoiReport;
+import java.util.List;
 
-// public interface RoiService {
-//     RoiReport generateRoiForCode(Long codeId);
-//     RoiReport getReportById(Long id);
-//     RoiReport getReportsForInfluencer(Long influencerId);
-//     RoiReport getReportsForCampaign(Long campaignId);
-// }
- 
+import com.example.demo.model.RoiReport;
+
+public interface RoiService {
+
+    RoiReport generateRoiForCode(Long codeId);
+
+    RoiReport getReportById(Long id);
+
+    List<RoiReport> getReportsForInfluencer(Long influencerId);
+
+    List<RoiReport> getReportsForCampaign(Long campaignId);
+}
