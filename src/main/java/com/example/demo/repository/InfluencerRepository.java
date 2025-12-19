d507d6aefb4cb4808dab8e846d2119307f10fd2f
bala-amypo <bala@amypo.in>
2025-12-19 04:38:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/InfluencerRepository.java
+++ src/main/java/com/example/demo/repository/InfluencerRepository.java
@@ -1,9 +1,9 @@
-package com.example.ROI.repository;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.ROI.model.Influencer;
+import com.example.demo.model.Influencer;
 @Repository
 public interface InfluencerRepository extends JpaRepository<Influencer,Long>{
 
