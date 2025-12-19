715cc29b19bba11e2f22d3449a5645d8f13ff13e
bala-amypo <bala@amypo.in>
2025-12-19 16:37:11 +0000

    User commit

--- src/main/java/com/example/demo/service/DiscountCodeService.java
+++ src/main/java/com/example/demo/service/DiscountCodeService.java
@@ -1,19 +1,21 @@
 package com.example.demo.service;
 
 import java.util.List;
-
 import com.example.demo.model.DiscountCode;
 
 public interface DiscountCodeService {
-    DiscountCode creaDiscountCode(DiscountCode code);
-    DiscountCode updaDiscountCode(Long id,DiscountCode code);
-    DiscountCode getCodeById(Long id);
-    DiscountCode getCodesByInfluencer(Long influencerId);
-    DiscountCode getCodesByCampaign(Long campaignId);
-    boolean deactivateCode(Long id);
-    DiscountCode createDiscountCode(DiscountCode code);
-    List<DiscountCode> getAllCodes();
-    DiscountCode updateDiscountCode(Long id, DiscountCode updatedCode);
 
-    
-} 
+    DiscountCode createDiscountCode(DiscountCode code);
+
+    DiscountCode updateDiscountCode(Long id, DiscountCode code);
+
+    DiscountCode getCodeById(Long id);
+
+    List<DiscountCode> getAllCodes();
+
+    List<DiscountCode> getCodesByInfluencer(Long influencerId);
+
+    List<DiscountCode> getCodesByCampaign(Long campaignId);
+
+    boolean deactivateCode(Long id);
+}
