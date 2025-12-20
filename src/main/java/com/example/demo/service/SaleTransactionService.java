268cf15b4ce75c1644f9c6f4d9fc7860784b772a
bala-amypo <bala@amypo.in>
2025-12-20 15:32:01 +0000

    User commit

--- src/main/java/com/example/demo/service/SaleTransactionService.java
+++ src/main/java/com/example/demo/service/SaleTransactionService.java
@@ -1,17 +1,17 @@
 package com.example.demo.service;
 
 import java.util.List;
-
 import com.example.demo.model.SaleTransaction;
 
 public interface SaleTransactionService {
+
     SaleTransaction logTransaction(SaleTransaction transaction);
+
     SaleTransaction getTransactionById(Long id);
-    SaleTransaction getSalesForCode(Long codeId);
-    SaleTransaction getSalesForInfluencer(Long influencerId);
-    SaleTransaction getSalesForCampaign(long campaignId);
-    SaleTransaction createTransaction(SaleTransaction transaction);
-    List<SaleTransaction> getAllTransactions();
-    boolean deleteTransaction(Long id);
-    
+
+    List<SaleTransaction> getSalesForCode(Long codeId);
+
+    List<SaleTransaction> getSalesForInfluencer(Long influencerId);
+
+    List<SaleTransaction> getSalesForCampaign(Long campaignId);
 }
