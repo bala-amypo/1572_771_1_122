4c79de421149a631547ef0d4da547fe1bd3d4cd6
bala-amypo <bala@amypo.in>
2025-12-20 07:51:57 +0000

    User commit

--- src/main/java/com/example/demo/repository/DiscountCodeRepository.java
+++ src/main/java/com/example/demo/repository/DiscountCodeRepository.java
@@ -1,16 +1,14 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.model.DiscountCode;
 
 @Repository
 public interface DiscountCodeRepository extends JpaRepository<DiscountCode, Long> {
 
-    List<DiscountCode> findByInfluencerId(Long influencerId);
+    Optional<DiscountCode> findByCode(String code);
 
-    List<DiscountCode> findByCampaignId(Long campaignId);
+    boolean existsByCode(String code);
 }
