b60f4bb8fb1166aa465895a6a1d3a5235254cd85
bala-amypo <bala@amypo.in>
2025-12-24 05:10:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/SaleTransactionController.java
+++ src/main/java/com/example/demo/controller/SaleTransactionController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.model.SaleTransaction;
 import com.example.demo.service.SaleTransactionService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/sales")
 public class SaleTransactionController {
 
     private final SaleTransactionService service;
@@ -14,11 +16,13 @@ public class SaleTransactionController {
         this.service = service;
     }
 
-    public ResponseEntity<SaleTransaction> createSale(SaleTransaction tx) {
-        return ResponseEntity.ok(service.createSale(tx));
+    @PostMapping
+    public SaleTransaction create(@RequestBody SaleTransaction sale) {
+        return service.createSale(sale);
     }
 
-    public ResponseEntity<List<SaleTransaction>> getSalesForCode(Long id) {
-        return ResponseEntity.ok(service.getSalesForCode(id));
+    @GetMapping
+    public List<SaleTransaction> getAll() {
+        return service.getAllSales();
     }
 }
