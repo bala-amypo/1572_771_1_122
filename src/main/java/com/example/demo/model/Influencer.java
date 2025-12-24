a46cbc24861bd36e7f6ab85e28369db2bc3b4355
bala-amypo <bala@amypo.in>
2025-12-24 04:05:47 +0000

    User commit

--- src/main/java/com/example/demo/model/Influencer.java
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -1,90 +1,16 @@
 package com.example.demo.model;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "socialHandle"))
 public class Influencer {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+    private boolean active;
 
-    private String socialHandle;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    private String email;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    private Boolean active = true;
-
-    private LocalDateTime createdAt;
-
-    @PrePersist
-    public void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-
-    public Influencer(){}
-
-    public Influencer(String name, String socialHandle, String email, Boolean active, LocalDateTime createdAt) {
-        this.name = name;
-        this.socialHandle = socialHandle;
-        this.email = email;
-        this.active = active;
-        this.createdAt = createdAt;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSocialHandle(String socialHandle) {
-        this.socialHandle = socialHandle;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSocialHandle() {
-        return socialHandle;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    
-
-    
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
 }
