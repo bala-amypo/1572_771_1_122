9b51676cf6fc8302026fa2b2a8717b21276e92fb
bala-amypo <bala@amypo.in>
2025-12-24 04:58:17 +0000

    User commit

--- src/main/java/com/example/demo/model/DiscountCode.java
+++ src/main/java/com/example/demo/model/DiscountCode.java
@@ -7,9 +7,6 @@ public class DiscountCode {
     private Influencer influencer;
     private Campaign campaign;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
     public String getCodeValue() { return codeValue; }
     public void setCodeValue(String codeValue) { this.codeValue = codeValue; }
 
--- src/main/java/com/example/demo/service/InfluencerService.java
+++ src/main/java/com/example/demo/service/InfluencerService.java
@@ -1,13 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Influencer;
+import com.example.demo.model.*;
 import java.util.List;
 
 public interface InfluencerService {
-
     Influencer createInfluencer(Influencer influencer);
-
     Influencer getInfluencerById(Long id);
-
     List<Influencer> getAllInfluencers();
 }
