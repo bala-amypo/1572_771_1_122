844e4b31bc8056a065aa8e2bb9d161bcb5ea019e
bala-amypo <bala@amypo.in>
2025-12-24 05:43:16 +0000

    User commit

--- src/main/java/com/example/demo/model/Influencer.java
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -3,14 +3,24 @@ package com.example.demo.model;
 public class Influencer {
     private Long id;
     private String name;
-    private boolean active;
+    private String socialHandle;
+    private boolean active = true;
+
+    public Influencer() {}
+
+    public Influencer(String name, String socialHandle, boolean active) {
+        this.name = name;
+        this.socialHandle = socialHandle;
+        this.active = active;
+    }
 
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
+    public String getSocialHandle() { return socialHandle; }
     public boolean isActive() { return active; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setSocialHandle(String socialHandle) { this.socialHandle = socialHandle; }
     public void setActive(boolean active) { this.active = active; }
 }
