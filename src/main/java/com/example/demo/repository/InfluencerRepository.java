35d1051a623084e52bab9311652d8998e1d99cdb
bala-amypo <bala@amypo.in>
2025-12-25 13:10:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/InfluencerRepository.java
+++ src/main/java/com/example/demo/repository/InfluencerRepository.java
@@ -1,10 +1,11 @@
-package com.example.demo.repository;
+package com.example.ROI.repository;
 
-import com.example.demo.model.Influencer;
+import com.example.ROI.model.Influencer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface InfluencerRepository extends JpaRepository<Influencer, Long> {
+
     Optional<Influencer> findBySocialHandle(String socialHandle);
 }
--- src/main/java/com/example/demo/service/InfluencerService.java
+++ src/main/java/com/example/demo/service/InfluencerService.java
@@ -1,16 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Influencer;
-
 import java.util.List;
 
 public interface InfluencerService {
 
     Influencer createInfluencer(Influencer influencer);
 
-    Influencer getInfluencer(Long id);
+    Influencer updateInfluencer(Long id, Influencer influencer);
+
+    Influencer deactivateInfluencer(Long id);
+
+    Influencer getInfluencerById(Long id);
 
     List<Influencer> getAllInfluencers();
-
-    void deactivate(Long id);
 }
